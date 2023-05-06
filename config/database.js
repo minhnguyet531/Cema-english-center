db4944a975162c3c6e53446911ca1b0db3f9d9da
minhnguyet531 <minhnguyet050301@gmail.com>
2023-05-06 23:02:05 +0700

    update database.js

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 // MONGO_URI=mongodb://localhost:27017/cemaenglishcenter ==> local
 // MONGO_URI=mongodb+srv://minhnguyet050301:Namba2001916@cemaenglishcenter.hyq5svu.mongodb.net/?retryWrites=true&w=majority  ==> mongooDB alats
-export const connectDB = async () => {
+export const connectDB = () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URI);
+        const { connection } = mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB Connected: ${connection.host}`);
     } catch (error) {
