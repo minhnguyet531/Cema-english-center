485ced4fbd518397f4bdec9d29b5956299eb76e3
minhnguyet531 <minhnguyet050301@gmail.com>
2023-05-07 00:23:05 +0700

    add connect database

--- config/database.js
+++ config/database.js
@@ -4,7 +4,10 @@ import mongoose from "mongoose";
 // MONGO_URI=mongodb+srv://minhnguyet050301:Namba2001916@cemaenglishcenter.hyq5svu.mongodb.net/?retryWrites=true&w=majority  ==> mongooDB alats
 export const connectDB = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URI);
+        const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
 
         console.log(`MongoDB Connected: ${connection.host}`);
     } catch (error) {
