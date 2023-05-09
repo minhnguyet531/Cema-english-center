14478b39014ddac524daefaf4da43257c458bba3
minhnguyet531 <minhnguyet050301@gmail.com>
2023-05-10 05:29:29 +0700

    fixed CORS

--- app.js
+++ app.js
@@ -22,6 +22,29 @@ app.use(
     })
 );
 
+app.use(function (req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = [
+        "http://localhost:3000",
+        "http://gamebrag.onrender.com",
+        "https://gamebrag.onrender.com",
+    ];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-credentials", true);
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, UPDATE"
+    );
+    next();
+});
+
 // All Routes Import
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
